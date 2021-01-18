841a8afe7bf9c372344f02cd26d87a89c070959c
praz <t.prazwal@gmail.com>
2021-01-18 10:07:27 +0100

    add GroceryItems component display the price and name of groceries in a table.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GroceryItems from './GroceryItems'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to my React App</p>
+      <GroceryItems />
+    </div>
+  );
+}
+
+export default App;
